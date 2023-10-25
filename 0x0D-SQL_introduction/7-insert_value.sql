e3a2c85390a4ad7dfa9297355f3f62a29a6d49ce
Galal-20 <ga71387@gmail.com>
2023-10-25 10:26:10 +0300

    up

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- insert a row in the table
-INSERT INTO first_table(id, name) VALUES(89, "Holberton School");
+-- Insert values
+-- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
